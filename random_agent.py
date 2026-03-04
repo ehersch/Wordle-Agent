4bb9c9c9c4da0e565efb7a26c06fc38b21d5980b
kantmanav <kantmanav@gmail.com>
2026-03-04 11:57:59 -0800

    Evaluate random agent using shaped reward function

--- random_agent.py
+++ random_agent.py
@@ -3,23 +3,39 @@ import gym_wordle
 
 env = gym.make('Wordle-v0')
 
-n_games = 100
+n_games = 200
 
 wins = 0
-avg_len = 0
+rewards = []
+lens = []
 for _ in range(n_games):
     obs = env.reset()
     done = False
+    reward = 0
     while not done:
         while True:
             # make a random guess
             act = env.action_space.sample()
 
             # take a step
-            obs, reward, done, _ = env.step(act)
-            break
+            raw, _, done, info = env.step(act)
 
-        env.render()
-    
+            # Match env-side per-character shaping from latest guess flags.
+            round_idx = env.unwrapped.round - 1
+            flags = raw[round_idx][5:10]
+            right_pos = getattr(env.unwrapped, "right_pos", 1)
+            wrong_pos = getattr(env.unwrapped, "wrong_pos", 2)
+            n_green = int((flags == right_pos).sum())
+            n_yellow = int((flags == wrong_pos).sum())
+            shaped = 0.25 * n_green + 0.10 * n_yellow - 0.05
+            reward += shaped
+            break
+    rewards.append(reward)
     rounds = env.unwrapped.round
-    print(rounds)
+    lens.append(rounds)
+
+avg_rew = sum(rewards) / n_games
+avg_len = sum(lens) / n_games
+
+print("Average reward: " + str(avg_rew))
+print("Average len: " + str(avg_len))
